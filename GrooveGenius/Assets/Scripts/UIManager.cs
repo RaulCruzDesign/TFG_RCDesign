152aa4d703b3aa96abab02a990b2b1009647c2d1
RaulCruzDesign <raulcruz037@gmail.com>
2024-06-24 10:40:14 +0200

    Ac5tualizacion UI

--- GrooveGenius/Assets/Scripts/UIManager.cs
+++ GrooveGenius/Assets/Scripts/UIManager.cs
@@ -10,6 +10,28 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI greatCountText;
     public TextMeshProUGUI goodCountText;
     public TextMeshProUGUI failCountText;
+    public TextMeshProUGUI timerText; // Campo de texto para el temporizador
+
+    private float startTime;
+
+    void Start()
+    {
+        InitializeTextFields();
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    private void InitializeTextFields()
+    {
+        UpdateTotalScore(0);
+        UpdateCombo(0);
+        UpdateMaxCombo(0);
+        UpdateNoteCounts(0, 0, 0, 0);
+    }
 
     public void UpdateTotalScore(int score)
     {
@@ -32,5 +54,15 @@ public class UIManager : MonoBehaviour
         greatCountText.text = "Geniales: " + great;
         goodCountText.text = "Buenos: " + good;
         failCountText.text = "Fallos: " + fail;
+    }
+
+    public void UpdateTimer()
+    {
+        float currentTime = Time.time - startTime;
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int milliseconds = Mathf.FloorToInt((currentTime * 1000) % 1000);
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
     }
 }
